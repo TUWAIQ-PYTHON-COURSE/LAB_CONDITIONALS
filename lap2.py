417a0975124f38cc9ee5451f89687818a77c46f9
nora <nora_abdullah33@outlook.com>
2022-06-28 18:41:30 +0300

    lap-2

---
+++ lap2.py
@@ -0,0 +1,13 @@
+
+movie : str = " Top gun " 
+rating  : int = 3
+popularity_score : float = 72.65 
+
+if rating >= 4 and popularity_score > 80 : 
+    print ("Highly recommended") 
+elif rating >= 3 and popularity_score > 70 :
+    print("I recommended it . It is good")
+elif rating <= 2 and  popularity_score > 60 : 
+    print("You should check it out!")
+elif rating <= 2 and  popularity_score < 50 : 
+    print("Don't watch it. It is a waste of time")
