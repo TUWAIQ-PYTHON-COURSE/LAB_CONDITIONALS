7eec0381783f2b021d76d5a0ff21cca0147a5e16
Saad <saadfahad8011@gmail.com>
2022-06-28 20:11:27 +0300

    Done

---
+++ LAB_conditional.py
@@ -0,0 +1,12 @@
+movie = "Fury"
+rating_of_the_movie:int = 3
+popularity_score:float =  72.65
+
+if rating_of_the_movie >= 4 and popularity_score > 80 :
+    print( "Highly recommended")
+elif rating_of_the_movie >= 4 and popularity_score > 70 :
+    print("I recommended it . It is good")
+elif rating_of_the_movie <= 2 and popularity_score > 60 :
+    print("You should check it out!")
+elif rating_of_the_movie <= 2 and popularity_score > 50 :
+        print("Don't watch it. It is a waste of time")
