e6fbde693f88d6f1af3c32cb50bed4b79e2a88ad
SillyRobot <51270428+SillyRobot883@users.noreply.github.com>
2022-06-28 18:20:56 +0300

    LAB2 - main.py modified

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ rating:int = 3
 
 popularity:float = 72.65
 
-if (rating > 4) or (popularity > 80):
+if (rating > 4) and (popularity > 80):
     print("Highly recommended.")
-elif (rating > 3) or (popularity > 70):
+elif (rating > 3) and (popularity > 70):
     print("I recommend it. It is good.")
-elif (rating > 2) or (popularity > 60):
+elif (rating > 2) and (popularity > 60):
     print("You should check it out!")
 else:
     print("Don't watch it. It is a waste of time.")
