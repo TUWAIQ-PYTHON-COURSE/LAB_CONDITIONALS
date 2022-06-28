ad79f13ee59ac84dba4246c09ec07e193517372c
anwaar <az.1221.anwar@gmail.com>
2022-06-28 17:21:49 +0300

    Lab3

---
+++ lab.py
@@ -0,0 +1,19 @@
+#Create a variable for the movie (choose any movie you like)
+movie : str = "top gun"
+#Create a variable of type int to hold the rating of the movie out of 5 . Give this movie 3
+rating: int = 5
+#Create a popularity score of type float , let it be 72.65
+score : float = 72.65
+#Using an if statement 
+#Check if the movie rating is 4 or greater and the popularity is greater than 80 , print "Highly recommended"
+if rating >= 4 and score > 80 :
+    print ("Highly recommended")
+#else if the movie rating is 3 or greater and the popularity is greater than 70 , print "I recommended it . It is good"
+elif rating >= 3 and score > 70 :
+    print ("I recommended it . It is good")
+#else if the movie rating is 2 or less and the popularity is greater than 60 , print "You should check it out!"
+elif rating <= 2 and score > 60 :
+    print ("You should check it out!")
+#else  the movie rating is 2 or less and the popularity is less than 50 , print "Don't watch it. It is a w
+else :
+    print("Don't watch it.  It is a waste of time")
