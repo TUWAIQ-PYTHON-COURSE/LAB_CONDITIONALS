1acb635104e1fa5af5b956d6aa19f28b2a145a9c
hashem-ali <hashem.bajabaa1@gmail.com>
2022-06-28 18:29:19 +0300

    change elif with else

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ elif rating >= 3 and score > 70:
     print("I recommended it. It is good")
 elif rating <= 2 and score > 60:
     print("You should check it out!")
-elif rating <=2 and score < 50:
+else:
     print("Don't watch it. It is a waste of time")   
