3d0ef0ac4fe35199d484e2eb5bd6e0c76f9ace8a
Yahya albariqi <kaizik.y18@gmail.com>
2022-06-28 19:07:11 +0300

    LAB_CONDITIONALS is completed main.py

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ elif movie_name == "Fast & Furious":
 if movie_rate == 4 and movie_score < 80 :
     print("Highly recommended")
 #- - else if the movie rating is 3 or greater and the popularity is greater than 70 , print "I recommended it . It is good"
-elif movie_rate == 3 or movie_score < 70 :
+elif movie_rate == 3 and movie_score < 70 :
     print("I recommended it . It is good")
 #- - else if the movie rating is 2 or less and the popularity is greater than 60 , print "You should check it out!"
-elif movie_rate == 2 or movie_score > 60 :
+elif movie_rate == 2 and movie_score > 60 :
     print("You should check it out!")
 #-  - else  the movie rating is 2 or less and the popularity is less than 50 , print "Don't watch it. It is a waste of time"
-elif movie_rate == 2 or movie_score < 50 :
+elif movie_rate == 2 and movie_score < 50 :
     print("Don't watch it. It is a waste of time")
 
 else:
