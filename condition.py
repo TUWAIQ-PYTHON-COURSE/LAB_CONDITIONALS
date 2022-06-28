9e0d62ebb1c14a22a192071f7f3bbb57e2d9b500
Yazeed Al-Harbi <yazeedal-harbi@Yazeeds-MacBook-Pro-4.local>
2022-06-28 19:23:44 +0300

    add conditinal

---
+++ condition.py
@@ -0,0 +1,24 @@
+
+'''
+product_price : int = 500
+
+if  product_price ==500 :
+    print ("the product is expensev!")
+elif product_price < 500 :
+     print ("the product is resnobale!")  
+elif product_price != 500 : 
+    print ("i'm not sure to buy")  
+'''
+
+first_name : str = "Naser"
+last_name : str = "Aharbi"
+
+if  first_name == "Naser" and last_name == "alotibi" :
+    print ("he is my cousin!")
+elif  first_name != "Naser" or last_name == "alotibi" :
+    print ("he is maybe cousin!")
+elif  first_name == "Naser" or not last_name != "alharbi" :
+    print ("maybe he is cusin!")
+else:
+   print ("i don't know!")
+
